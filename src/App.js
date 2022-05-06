58fb8b1d397ac7975d0b1fcaa6b2af8a963f1e0c
SJ Im <im.sungjin.sean@gmail.com>
2022-05-06 16:06:10 -0700

    finish bbs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import BoardForm from './App_BoardForm';
+import BoardItem from './App_BoardItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World!!
-        </a>
-      </header>
-    </div>
-  );
+/*
+    component files.
+*/
+class App extends Component {
+
+    state = {
+        maxNo: 3,
+        boards: [
+            {
+                brdno: 1,
+                brdwriter: 'Lee SunSin',
+                brdtitle: 'If you intend to live then you die',
+                brddate: new Date()
+            },
+            {
+                brdno: 2,
+                brdwriter: 'So SiNo',
+                brdtitle: 'Founder for two countries',
+                brddate: new Date()
+            }
+        ],
+         selectedBoard:{}
+    }
+    
+    handleSaveData = (data) => {
+        if (!data.brdno) {            // new : Insert
+            this.setState({
+                maxNo: this.state.maxNo+1,
+                boards: this.state.boards.concat({brdno: this.state.maxNo, brddate: new Date(), ...data }),
+                selectedBoard: {}
+            });
+        } else {                                                        // Update
+            this.setState({
+                boards: this.state.boards.map(row => data.brdno === row.brdno ? {...data }: row),
+                selectedBoard: {}
+            })            
+        }
+    }
+    
+    handleRemove = (brdno) => {
+        this.setState({
+            boards: this.state.boards.filter(row => row.brdno !== brdno)
+        })
+    }
+    
+    handleSelectRow = (row) => {
+        this.setState({selectedBoard:row});
+    }
+    
+    render() {
+        const { boards, selectedBoard } = this.state;
+
+        return (
+            <div>
+                <BoardForm selectedBoard={selectedBoard} onSaveData={this.handleSaveData}/>
+                <table border="1">
+                    <tbody>
+                    <tr align="center">
+                        <td width="50">No.</td>
+                        <td width="300">Title</td>
+                        <td width="100">Name</td>
+                        <td width="100">Date</td>
+                    </tr>
+                    {
+                        boards.map(row =>
+                            (<BoardItem key={row.brdno} row={row} onRemove={this.handleRemove} onSelectRow={this.handleSelectRow} />)
+                        )
+                    }
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
 
 export default App;
